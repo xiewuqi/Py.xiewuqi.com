c155265896e405de1af274c7dadd7f54398cb00d
谢武琦 <xiewuqi@cpe-172-100-40-195.twcny.res.rr.com>
2017-06-13 17:16:18 +0800

    add ex2-15

---
+++ ex12.py
@@ -0,0 +1,7 @@
+age = raw_input("How old are you?")
+height = rwa_input("How tall are you?")
+weight = raw_ input("How much do you weigh?")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+    
---
+++ ex2.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# 中文注释
+# A comment,this is so you can read your program later.
+# 再来一个,这是你能阅读的信
+# Anything afetr the # is ignored by python.
+
+
+print "I could have code like this." # and the comment afetr is ignored
+
+# You can also usw a comment to "disabole" or commnet out a piece of code:
+# print "This won't run."
+
+print "This will run."
