53c29d534c830bc5e4306211e97c0cbbba918d01
谢武琦 <xiewuqi@cpe-172-100-30-104.twcny.res.rr.com>
2017-06-20 15:39:48 +0800

    修改 ex41.py

--- ex41.py
+++ ex41.py
@@ -2,70 +2,70 @@ import random
 from urllib import urlopen
 import sys
 
-WORD_URL = "http//learncodethehardway.org/words.txt"
-WORD = []
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
 
 PHRASES = {
-    "calss %%%(%%%):":
-    "Make calss named %%% that is-a %%%.",
-    "class %%%(object):\n\tdef __init__ (self, ***)":
-     "class %%% has-a __init__ that takes self and *** paramerters.",
-    "class %%% (object):\n\tdef ***(self, @@@)"
+    "class %%%(%%%):":
+        "Make class named %%% that is-a %%%.",
+    "class %%%(object):\n\tdef __init__(self, ***)":
+        "class %%% has-a __init__ that takes self and *** parameters.",
+    "class %%%(object):\n\tdef ***(self, @@@)":
         "class %%% has-a function named *** that takes self and @@@ parameters.",
     "*** = %%%()":
         "Set *** to an instance of class %%%.",
     "***.***(@@@)":
         "From *** get the *** function, and call it with parameters self , @@@.",
     "***.*** = '***'":
-        "From *** get the *** atteibute and set it to '***'."
+        "From *** get the *** attribute and set it to '***'."
 }
 
 # do the want to drill pharases first
 PHRASE_FIRST = False
-if len(sys.argv) == 2 and sys.argv[1] == "englist":
-    PHRASE_FITST = True
+if  len(sys.argv) == 2 and sys.argv[1] == "english":
+        PHRASE_FIRST = True
 
 # ioad up the words from the website
-for word in urlopen (WORD_URL).readlines():
+for word in urlopen(WORD_URL).readlines():
     WORDS.append(word.strip())
 
     
-def coonvert (snippet, pharase):
-    class_name = [w.capitalize()for w in
-                  random.sample(WORS, snippet,count("***"))]
-    other_name = random.sample(WORDS, snippet,count("***")
+def convert (snippet, phrase):
+    class_names = [w.capitalize() for w in
+                  random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
 
-    for i in randge(0, snippet.count("@@@")):
+    for i in range(0, snippet.count("@@@")):
         param_count = random.randint(1, 3)
-        param_names,append(', '.join(random.sample(WORDS, param_count)))
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
-        result = sentencd[:]
+        result = sentence[:]
         
         # fake calss names
-        for word in class_name:
-            result = result,relpace("%%%", word, 1)
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
 
         # fake other names
         for word in other_names:
-            prsult = resust.replace("***", word, 1)
+            result = result.replace("***", word, 1)
             
         # fake parameter lists
         for word in param_names:
-            result = result,replace("@@@", word, 1)
+            result = result.replace("@@@", word, 1)
             
-            result.append(result)            
+        results.append(result)            
         
-        return results
+    return results
         
          
 # keep going until the hit GTRl-D
 try:
     while True:
-        snoppets = PHRASES.keys()
-        ranom,shuffle(snippets)
+        snippets = PHRASES.keys()
+        random.shuffle(snippets)
         
         for snippet in snippets:
             phrase = PHRASES[snippet]
@@ -75,7 +75,7 @@ try:
                 
             print question
             
-            rwa_input(">")
-            print "ANSWER:  %s\n\n" % answer
-except EOOFError:
+            raw_input("> ")
+            print "ANSWER: %s\n\n" % answer
+except EOFError:
     print "\nBye"
