b9e89fe91fb39e43c4b30d1b224346d28f478879
谢武琦 <xiewuqi@cpe-172-100-30-104.twcny.res.rr.com>
2017-06-20 17:28:00 +0800

    add ex42.py

--- ex42.py
+++ ex42.py
@@ -3,31 +3,31 @@
 class Animal(object):
     pass
     
-## Dog is-a Animal??
+## Dog is-a Animal
 class Dog(Animal):
 
     def __init__(self, name):
     ## ??
-    self.name = name
+        self.name = name
 
-## Cat is-a Animal??
+## Cat is-a Animal
 class Cat(Animal):
     
     def __init__(self, name):
         ## ??
         self.name = name
         
-## ?
+## person is-a object?
 class Person (object):
     
     def __init__(self, name):
-        ## ??
+        ## Person has-a name ??
         self.name = name
         
         ## Person has-a pet of some kind
         self.pet = None
         
-## Employee（雇员）??
+## Employee（雇员）is-a person??
 class Employee(Person):
     
     def __init__(self, name, salary):
@@ -36,46 +36,46 @@ class Employee(Person):
         ## ？？
         self.salary = salary
         
-## ??
+## fish is-a object??
 class Fish(object):
     pass
 
-##  三文鱼??
+## 三文鱼 is-a fish??
 class Salmon(Fish):
     pass
     
-## 大比目鱼??
-class Hilibut(Fish):
+## 大比目鱼 is-a fish??
+class Halibut(Fish):
     pass
     
     
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## satan is-a Cat??
 satan = Cat("Satan")
 
-## ??
+## maer is-a person??
 mary = Person("Mary")
 
-## def ??
+## marry.pet is-a satan ??
 mary.pet = satan
 
-## ??
+## frank is-a Emoloyee has-a Frank, 120000??
 frank = Employee("Frank", 120000)
 
-## ？
+## frnk.pet is-a rover
 frank.pet = rover
 
-## ?
+# 有鱼鳍的是雨 ?
 flipper = Fish()
 
-## ?
-crous = Salmon()
+## crouse is-a 三文鱼 ?
+crouse = Salmon()
 
-## ??    
+## harry is-a 大比目鱼 ??    
 harry = Halibut()
 
-print "怎样确定脚本运行成功呢？"
+
 
 
