403bab80118dafd47fdaaede05b336debe85f9e7
谢武琦 <xiewuqi@cpe-172-100-50-176.twcny.res.rr.com>
2017-06-21 17:13:39 +0800

    适应py3.6 6-10.py

--- ex8.py
+++ ex8.py
@@ -1,12 +1,12 @@
-formatter = "%r %r %r %r"
+formatter = "{} {} {} {}"
 
-print formatter % (1, 2, 3, 4) 
-print formatter % ("noe", "two", "three", "four")
-print formatter % (True, False, False, True)
-print formatter % (formatter, formatter, formatter, formatter)
-print formatter % (    
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("noe", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(    
 	"I had this thing.",
 	"That you could type up right.",
 	"But it did't sing.",
 	"So I said goodnight."
-)
+))
