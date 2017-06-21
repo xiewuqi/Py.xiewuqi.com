9913c076754f8f38eee6a8c0e4c09f17fe6db6d8
谢武琦 <xiewuqi@cpe-172-100-50-176.twcny.res.rr.com>
2017-06-21 15:31:34 +0800

    修改 ex6.py

--- ex6.py
+++ ex6.py
@@ -1,20 +1,21 @@
-x = "There are %d types of people." % 10
+types_of_people = 10
+x = f"There are {types_of_people} types of people." 
 binary = "binray"
 do_not = "don't"
-y = "Those who know %s  and those who %s." % (binary, do_not)
+y = f"Those who know {binary}  and those who {do_not}." 
 
-print x
-print y
+print(x)
+print(y)
 
-print "I said:%r." % x
-print "I also said: '%s'." % y
+print(f"I said:{x}.") 
+print(f"I also said: '{y}'.") 
 
 hilarious = False
-joke_evaluation = "Isn't that joke so funny?! %r"
+joke_evaluation = "Isn't that joke so funny?! {}"
 
-print joke_evaluation % hilarious
+print (joke_evaluation.format(hilarious))
 
 w = "This is the left side of ..."
 e = "a string with a right side."
 
-print w+e
+print(w+e)
