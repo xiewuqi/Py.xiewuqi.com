857dca50b10dbc2a2714b3578290a89a28f83457
谢武琦 <xiewuqi@cpe-172-100-40-194.twcny.res.rr.com>
2017-06-22 18:47:08 +0800

    py3.6  36-37

--- ex36.py
+++ ex36.py
@@ -2,39 +2,42 @@
 from sys import exit
 
 def deng_room():
-    print "你碰见了灯神"
-    print "他让你许一个愿望"
+    print("你碰见了灯神")
+    print("他让你许一个愿望")
     
-    next = raw_input(">") 
-    if next == "出口在哪?":
-        print "一直往右走孩子。"
+    next = input(">") 
+    if next == "出口在哪":
         chukou_room()
+        
+        
     elif next == "给我一个美女":
-        print "不在权限范围之内，886"
+        print("不在权限范围之内，886")
     else:
-        print "好吧，你饿死在这个房间！"
+        print("好吧，你饿死在这个房间！")
         
 def chukou_room():
-    print "你到了出口"
+    print('我送你到出口')
+    print('----------')    
+    print("你到了出口")
 
 def kong_room():
-    print "这个房间看起来空无一物"
+    print("这个房间看起来空无一物")
     
 def sitou_room():
-    print "好吧，你掉了下去，摔了个稀巴烂。"
+    print("好吧，你掉了下去，摔了个稀巴烂。")
 
 def dead(why):
-    print why, "好可怜"
+    print(hy, "好可怜")
     
 def start():
-    print """
+    print("""
 > 哦，你不小心踩入了一个陷阱
 ······
 许久，你从昏迷中醒来。
 发现自己在一个幽暗的小屋，四周个有一扇门。
-你准备去那扇门呢？ """
+你准备去那扇门呢？ """)
 
-    next = raw_input(">")
+    next = input(">")
     if next == "左边":
 	    deng_room()
     elif next == "右边":
--- ex37.py
+++ ex37.py
@@ -4,46 +4,47 @@
 - del 删除操作 list  例子： a=[s, d, sd,] del[2] 删除第二个元素
 - from 一般用与参数导入 一般用作 from xx  import xx
 - not 布尔逻辑 非 例子，非真即假 非假即真
-- while  True 条件下循环， False 条件停止
+- while  循环，True 条件下循环， False 条件停止
 - as  import  xxx as xxx
 - elif 如果另外一种结果
 - global  全局变量
 - or 布尔逻辑  只要有 True 就为真
-- with  
-- assert
-- else 
+- with  写入文件
+- assert  断言语句用来声明某个条件是真的，如果为假 就显示异常
+- else 如果 if elif 都不是，执行else：
 - if  如果的意思
-- pass
-- yield
-- break
+- pass 通过 ，表示不做任何事，占位符
+- yield 
+- break 用来终止循环语句
 - except
-- import
-- print
-- class
+- import  导入文件  xx import 或者 for xx in import xx
+- print  打印 
+- class  类
 - exec
 - in
 - raise
 - continue
 - finally
 - is
-- return
-- def
+- return   返回一个函数值
+- def 定义函数 例子：def move(): 
 - for  循环 
     - 例子：a = [1, 2, 3,]  for a in a  
 
 - lambda
 - try
+- pop（） 删除某个字节
 
 
 ## 数据类型
 
-- True
-- False
-- None
+- True   真值
+- False  假值
+- None   判断为空
 - strings
-- numbers
+- numbers  数字
 - floats
-- lists
+- lists   列表
 
 
 ## 字符串转义序列
