1c22548923571f7e745210097ff5e1c8f8cd62fe
谢武琦 <xiewuqi@cpe-172-100-50-87.twcny.res.rr.com>
2017-06-28 15:56:43 +0800

    增加 内容

--- ex37.py
+++ ex37.py
@@ -6,33 +6,33 @@
 - not 布尔逻辑 非 例子，非真即假 非假即真
 - while  循环，True 条件下循环， False 条件停止
 - as  import  xxx as xxx
-- elif 如果另外一种结果
-- global  全局变量
+- elif   其他 如果另外一种结果
+- global    全球 申请一个全局变量
 - or 布尔逻辑  只要有 True 就为真
-- with  写入文件
+- with  一个变量的别名 	with X as Y: pass
 - assert  断言语句用来声明某个条件是真的，如果为假 就显示异常
 - else 如果 if elif 都不是，执行else：
 - if  如果的意思
 - pass 通过 ，表示不做任何事，占位符
-- yield 
-- break 用来终止循环语句
-- except
+- yield  暂停， 返回给调用者	def X(): yield Y; X().next()
+- break   打破 用来终止循环语句
+- except  如果发生了异常请执行此操作  except ValueError，e：print（e）
 - import  导入文件  xx import 或者 for xx in import xx
 - print  打印 
 - class  类
-- exec
-- in
-- raise
-- continue
-- finally
-- is
+- exec 运行一个字符串作为Python。	exec'print （“hello”）'
+- in   	一部分for循环。也是X中的X的测试。	在Y X：通过也在[1] == TRUE 1
+- raise 代码出错时，抛出一个异常   raise ValueError("No")
+- continue 停止这一次循环，但继续下一次循环	while True: continuev
+- finally  最后  无论怎么，最后执行xx
+- is   	喜欢==    例子： 1 is 1 == True
 - return   返回一个函数值
 - def 定义函数 例子：def move(): 
 - for  循环 
     - 例子：a = [1, 2, 3,]  for a in a  
 
-- lambda
-- try
+- lambda 创建一个无名函数	s = lambda y: y ** y; s(3)
+- try  尝试
 - pop（） 删除某个字节
 
 
